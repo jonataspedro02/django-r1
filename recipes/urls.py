fdbc69d646a88c1671207e4854b4db384c4109f4
jonataspedro02 <p.jonatas@ifrn.edu.br>
2023-01-29 12:39:40 -0300

    v0.0.1

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from recipes.views import home, sobre, contatos, receitas
+from recipes.views import index, sobre, contatos, contatos_2, contatos_3, receitas, receitas_2, receitas_3
 
 urlpatterns = [
-    path('', home),
+    path('', index),
     path('sobre/', sobre),
     path('contatos/', contatos, name='contatos'),
-    path('receita/', receitas, name='receitas')
+    path('contatos_2/', contatos_2, name='contatos_2'),
+    path('contatos_3/', contatos_3, name='contatos_3'),
+    path('receitas/', receitas, name='receitas'),
+    path('receitas_2/', receitas_2, name='receitas_2'),
+    path('receitas_3/', receitas_3, name='receitas_3')
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -5,15 +5,27 @@ from django.http import HttpResponse
 # Create your views here
 # HTTP RESPONSE
 
-def home(request):
-    return render(request, 'recipes/pages/home.html', context={'name': 'Jonatas Pedro'})
+def index(request):
+    return render(request, 'recipes/pages/index.html', context={'name': 'Shelbys'})
 
 def sobre(request):
     return render(request,'sobre.html')
 
 def contatos(request):
-  return render(request, 'recipes/pages/contatos.html', context={'name': 'Jonatas Pedro'})
+  return render(request, 'recipes/pages/contatos.html', context={'name': 'Shelbys'})
 
+def contatos_2(request):
+  return render(request, 'recipes/pages/contatos_2.html', context={'name': 'Shelbys'})
+
+def contatos_3(request):
+  return render(request, 'recipes/pages/contatos_3.html', context={'name': 'Shelbys'})
 def receitas(request):
-    return render(request, 'recipes/pages/receitas.html', context={'name': 'Jonatas Pedro'})
+    return render(request, 'recipes/pages/receitas.html', context={'name': 'Shelbys'})
+
+def receitas_2(request):
+    return render(request, 'recipes/pages/receitas_2.html', context={'name': 'Shelbys'})
+
+def receitas_3(request):
+    return render(request, 'recipes/pages/receitas_3.html', context={'name': 'Shelbys'})
+
 # Create your views here.
